fd797a12528c58d18c84f5cc89f763c339ce1816
Mray000 <ainurhabibullin0@gmail.com>
2021-04-23 15:40:34 +0300

    refactor

--- popup.js
+++ popup.js
@@ -1,16 +1,24 @@
 window.onload = () => {
   chrome.storage.local.get(null, (data) => {
     const container = document.getElementById("container");
-    Object.keys(data).forEach((chanel) => {
-      const div = document.createElement("div");
-      div.style.display = "flex";
-      div.style.backgroundColor = "white";
-      div.style.borderRadius = "5px";
-      div.style.marginTop = "5px";
-      div.style.paddingLeft = "3px";
-      div.style.fontSize = "17px";
-      div.innerHTML = `<img style="width: 30px; height: 30px; border-radius: 100%; margin-right: 5px;" src=${data[chanel][1]} /><span style="line-height: 1.5">${chanel}: ${data[chanel][0]}</span>`;
-      container.appendChild(div);
-    });
+    let keys = Object.keys(data);
+    if (keys.length) {
+      keys.forEach((chanel) => {
+        const div = document.createElement("div");
+        div.style.display = "flex";
+        div.style.backgroundColor = "white";
+        div.style.borderRadius = "5px";
+        div.style.marginTop = "5px";
+        div.style.paddingLeft = "3px";
+        div.style.fontSize = "16px";
+        div.style.fontWeight = "500";
+        div.style.color = "#202020";
+        div.style.padding = "4px";
+        div.innerHTML = `<img style="width: 25px; height: 25px; border-radius: 100%; margin-right: 5px;" src=${data[chanel][1]} /><span style="line-height: 1.5">${chanel}: ${data[chanel][0]}</span>`;
+        container.appendChild(div);
+      });
+    } else {
+      document.body.innerText = "Start watch video!";
+    }
   });
 };
